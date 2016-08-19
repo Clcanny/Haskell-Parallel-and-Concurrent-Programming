4fd1f117a51608de22d278ffd30c4f455fef8bc4
demons <837940593@qq.com>
2016-08-19 11:43:08 +0800

    .

--- ch12/myServer/server3.hs
+++ ch12/myServer/server3.hs
@@ -1,19 +1,28 @@
-import ConcurrentUtils
+-- 这个版本的问题是：
+-- 如果一个客户端改变了factor之后，下一个客户端才连接进来；
+-- 此时新客户端的factor仍然是2，直到有新的factor输入。
+
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import ConcurrentUtils (forkFinally)
 import Network
-import Control.Monad
+import Control.Monad (forever)
 import Control.Concurrent (forkIO)
-import System.IO
-import Text.Printf
-import Control.Exception
-import Control.Concurrent.STM.TChan
+import System.IO (Handle, BufferMode(..), hClose, hSetBuffering, hGetLine, hPutStr, hPutStrLn)
+import Text.Printf (printf)
+import Control.Exception (SomeException, handle)
+import Control.Concurrent.MVar (newMVar, takeMVar, putMVar, readMVar)
+import Control.Concurrent.Chan (Chan, newChan, readChan, writeChan, isEmptyChan, dupChan)
 
 -- <<main
 main = withSocketsDo $ do
   sock <- listenOn (PortNumber (fromIntegral port))              
   printf "Listening on port %d\n" port
+  chans <- newChan
   forever $
     accept sock >>= \(handler, host, port) ->
     printf "Accepted connection from %s: %s\n" host (show port) >>
+    dupChan chans >>= \chans' ->
     let
       exceptionHandler r = 
         case r of
@@ -24,40 +33,55 @@ main = withSocketsDo $ do
             printf "Meet an End.\n" >> 
             hClose handler
     in
-      forkFinally (talk' handler) exceptionHandler
+      forkFinally (talk handler (State chans')) exceptionHandler
 
 port :: Int
 port = 44444
 -- >>
 
 newtype State = State {
-  broadcastTChan :: TChan Int
+  broadcastChan :: Chan Integer
 }
 
-talk' :: Handle -> IO ()
-talk' h = do
-  hSetBuffering h LineBuffering
-  loop
- where
-  loop =
-    hGetLine h >>= \line ->
-    handle 
-      (\e -> 
-        print (e :: SomeException) >> 
-        hPutStrLn h "Please input numbers or \"end\"." >> 
-        loop) $
-      if line == "end"
-        then hPutStrLn h "Thank u for using the Haskell doubling service."
-        else
-          if head line == '*'
-            then 
-              let 
-                factor = broadcastTChan state
-                newFactor = read (drop 1 line) :: Integer
-              in
-                printf "factor: %d\n" newFactor >>
-                atomically $ writeTChan factor newFactor
-            else
-
-              hPutStrLn h (show (2 * (read line :: Integer))) >>
-              loop
+talk :: Handle -> State -> IO ()
+talk h state =
+  hSetBuffering h LineBuffering >>
+  newMVar 2 >>= \factor ->
+  let
+    loop =
+      hGetLine h >>= \line ->
+      handle 
+        (\e -> 
+          print (e :: SomeException) >> 
+          hPutStrLn h "Please input numbers or \"end\"." >> 
+          loop) $
+        if line == "end"
+          then 
+            hPutStrLn h "Thank u for using the Haskell doubling service."
+          else
+            if head line == '*'
+              then 
+                let
+                  newFactor = read (drop 1 line) :: Integer
+                in
+                  printf "%d\n" newFactor >>
+                  writeChan (broadcastChan state) newFactor >>
+                  loop
+              else
+                let 
+                  chans = broadcastChan state
+                in
+                  isEmptyChan chans >>= \isEmpty ->
+                  case isEmpty of
+                    True -> 
+                      readMVar factor >>= \factor' -> 
+                      hPutStrLn h (show (factor' * read line ::Integer)) >> 
+                      loop
+                    False -> 
+                      readChan chans >>= \newFactor -> 
+                      takeMVar factor >> 
+                      putMVar factor newFactor >>
+                      hPutStrLn h (show (newFactor * (read line :: Integer))) >>
+                      loop
+  in 
+    loop
